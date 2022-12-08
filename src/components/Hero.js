f6cce84769519efacfa92b95ae3a2e11f78e2622
Derrion-Malik Hall <dmalikhall17@gmail.com>
2022-12-08 17:56:14 -0500

    changed elevator pitch

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
         <main className='hero-container' id='hero'>
             <section className='my-info'>
                 <h2>Hello I'm Derrion Hall</h2>
-                <p>Front-End Developer.
-                    I specialize in React Js  and  Responisive Design. I have a passion for web development and I am driven to reach my fullest potential.</p>
+                <p>Web Developer.
+                    Using Wordpress, HTML, CSS, Javascript, and React to create responsive websites. I have a passion for web development and I am driven to reach my fullest potential.</p>
 
                 <div className="hero-button">
                     <Link to="projects" spy={true} smooth={true} offset={-80} duration={500}>
